ce6a5539b9755bfa9b409209b6b2020cbaf4997c
kmacocco <macocco@phimeca.com>
2017-05-31 12:16:46 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,6 +1,13 @@
 from __future__ import division
+import numpy as np
+
 def test_addition():
     assert 1 + 1 == 2
 
 def test_division():
     assert 3/2 == 1.5
+
+def test_numpy_part():
+    input_arr = np.array([3, 2, -1, 10])
+    partitioned = np.partition(input_arr, 1)
+    assert partitioned[1] == 2
