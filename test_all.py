94e73349049a2e644df52e3609a7b0847c737824
kmacocco <macocco@phimeca.com>
2017-05-31 15:38:43 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,13 +1,22 @@
-from __future__ import division
 import numpy as np
+
 
 def test_addition():
     assert 1 + 1 == 2
 
-def test_division():
-    assert 3/2 == 1.5
 
-def test_numpy_part():
-    input_arr = np.array([3, 2, -1, 10])
-    partitioned = np.partition(input_arr, 1)
-    assert partitioned[1] == 2
+def test_division():
+    assert 3./2 == 1.5
+
+
+def partition_wrapper(arr, kth):
+    try:
+        return np.partition(arr, kth)
+    except AttributeError:
+        return np.sort(arr)
+
+
+def test_partition():
+    input_array = np.array([-10, 3, 1, 5, -7])
+    partitioned_array = partition_wrapper(input_array, 2)
+    assert partitioned_array[2] == 1
